a45332c4858c3e5696e4b2d2f6609e988139ca16
Santiago <santiagofig00@gmail.com>
2022-11-03 20:29:42 -0300

    Agrego clase estudiante y corrijo logica para agregar estudiantes

---
+++ js/classEstudiante.js
@@ -0,0 +1,8 @@
+export class Estudiante{
+    constructor(nombre, apellido, legajo, nota){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+        this.nota = nota;
+    }
+}
--- js/estudiantes.js
+++ js/estudiantes.js
@@ -1,21 +1,40 @@
+import { Estudiante } from "./classEstudiante.js";
+
+let estudiante1 = new Estudiante('Elio', 'Lazarte', 52550, 4);
+let estudiante2 = new Estudiante('Lautaro', 'Rodriguez', 53555, 8);
+let estudiante3 = new Estudiante('Agucho', 'Mortadelli', 52147, 1);
+
+let listaEstudiante = [estudiante1, estudiante2, estudiante3];
+let tablaEstudiante = document.querySelector("#tablaEstudiante");
+
+cargaTabla();
+
 let nombre = document.querySelector("#nombre");
 let apellido = document.querySelector("#apellido");
 let legajo = document.querySelector("#legajo");
 let nota = document.querySelector("#nota");
-let tablaEstudiante = document.querySelector("#tablaEstudiante");
 let formEstudiante = document.querySelector("#formEstudiante");
 
-formEstudiante.addEventListener("submit", dibujarTabla);
+formEstudiante.addEventListener("submit", agregarEstudiante);
 
-console.log(tablaEstudiante)
-function dibujarTabla (e) {
-  e.preventDefault();
-  tablaEstudiante.innerHTML = "";
-  tablaEstudiante.innerHTML += ` <tr>
-    <th scope="row">${legajo.value}</th>
-    <td>${apellido.value}</td>
-    <td>${nombre.value}</td>
-    <td>${nota.value}</td>
+function cargaTabla() {
+tablaEstudiante.innerHTML ='';
+  listaEstudiante.map((estudiante) => {
+    tablaEstudiante.innerHTML += ` <tr>
+    <th scope="row">${estudiante.legajo}</th>
+    <td>${estudiante.apellido}</td>
+    <td>${estudiante.nombre}</td>
+    <td>${estudiante.nota}</td>
     <td><button class="btn btn-danger"><i class="bi bi-trash"></i></button></td>
   </tr>`;
-};
+  });
+}
+
+console.log(tablaEstudiante);
+function agregarEstudiante(e) {
+  e.preventDefault();
+  let nuevoEst = new Estudiante(nombre.value, apellido.value, legajo.value, nota.value);
+  listaEstudiante.push(nuevoEst);
+  cargaTabla();
+  formEstudiante.reset();
+}
